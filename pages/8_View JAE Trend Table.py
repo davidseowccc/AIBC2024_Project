3181d2184ead5a342095bf9cbc625bba4957dd51
davidseowccc <62555082+davidseowccc@users.noreply.github.com>
2024-10-19 19:57:17 +0800

    Update 8_View JAE Trend Table.py

--- pages/8_View JAE Trend Table.py
+++ pages/8_View JAE Trend Table.py
@@ -13,7 +13,7 @@ df = pd.read_csv(url, encoding="ISO-8859-1")
 df.index += 1
 
 st.title("NP JAE COPs 2014-2024 Table")
-st.text("This dataset is collated from past JAE/PFPAE data and scraped from \nPoly/JC COPs 2024 (10 year trend): https://docs.google.com/spreadsheets/d/14b5VLIr9W5teiOp0vCg86B6xxpFiwk8jeIzwbdJmWjU/pubhtml#")
+st.text("This dataset is collated from past JAE/PFPAE data and scraped from \nPoly/JC COPs 2024 (10 year trend): \nhttps://docs.google.com/spreadsheets/d/14b5VLIr9W5teiOp0vCg86B6xxpFiwk8jeIzwbdJmWjU/pubhtml#")
 
 # Display the DataFrame
 st.table(df)
