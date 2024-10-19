ccfb0cb9081f194ef76268aa166c5217e5db4cbe
davidseowccc <62555082+davidseowccc@users.noreply.github.com>
2024-10-19 14:50:32 +0800

    Update 1_Chatbot 1 JAE.py

--- pages/1_Chatbot 1 JAE.py
+++ pages/1_Chatbot 1 JAE.py
@@ -62,12 +62,14 @@ def get_completion_by_messages(messages, model="mixtral-8x7b-32768", temperature
 
 
 def generate_response(user_message):
-    # upload file
-    filepath = './data/table_np_csv.csv'
-    with open(filepath, 'r') as file:
-        # df = pd.read_csv(file)
-        df = pd.read_csv(file, encoding="ISO-8859-1")
-    
+    # # upload file
+    # filepath = './data/table_np_csv.csv'
+    # with open(filepath, 'r') as file:
+    #     # df = pd.read_csv(file)
+    #     df = pd.read_csv(file, encoding="ISO-8859-1")
+
+    url = 'https://raw.githubusercontent.com/davidseowccc/AIBC2024_Project/blob/main/data/table_np_csv.csv'
+    df = pd.read_csv(url, encoding="ISO-8859-1")
     product_details=df
 
     delimiter = "####"
